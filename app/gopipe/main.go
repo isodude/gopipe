1962d910f8942ffa69bec4eeb4a79faa9be053bd
Josef Johansson <josef@oderland.se>
2024-04-30 10:01:29 +0200

    Abort listener if errgroup recieves error
    Signed-off-by: Josef Johansson <josef@oderland.se>

--- app/gopipe/main.go
+++ app/gopipe/main.go
@@ -48,7 +48,7 @@ func main() {
 		connections = append(connections, connection)
 	}
 
-	bCtx, cancel := context.WithCancel(context.Background())
+	bCtx := context.Background()
 	g, ctx := errgroup.WithContext(bCtx)
 
 	for _, k := range connections {
@@ -89,11 +89,11 @@ func main() {
 		}
 
 		g.Go(func() error {
-			if err := k.Listen.Listen(&k.Client); err != nil {
-				cancel()
-				return err
+			err := k.Listen.Listen(&k.Client)
+			if err != nil && k.Debug {
+				fmt.Printf("Error: %v\n", err)
 			}
-			return nil
+			return err
 		})
 	}
 	if err := g.Wait(); err != nil {
--- lib/listen.go
+++ lib/listen.go
@@ -173,6 +173,10 @@ func (l *Listen) Listen(client *Client) (err error) {
 		}
 	}
 
+	go func() {
+		<- l.Ctx.Done()
+		l.listener.Close()
+	}()
 	for {
 		from, err = l.listener.Accept()
 		if err != nil {
